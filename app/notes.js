1db528e3c4964bdc8d2469f82c2ba3a47f465777
Osalotioman <osazuwaemmanuel25@gmail.com>
2024-05-23 00:15:02 +0100

    Updates

--- app/notes.js
+++ app/notes.js
@@ -1,3 +1,14 @@
 var notes_json = [
-  
+  {
+    "Course": "MTH 210(201) - Elementary Algebra and Analysis",
+    "PDF": "https://github.com/Osalotioman/notes/blob/main/uniben%2F200lv%2FFirstSemester%2FMTH%2FLectureNotes_MTH210%28201%29_10-05-2024_WrittenBy_Osalotioman.pdf",
+    "Info": "<br> Introduction to Set Theory",
+    "Lecturer": "<br>Lecture delivered by Lecturer : DR.I.C. Alufohai",
+    "Writer": "<br>Written by OsazuwaEmmanuelOsalotioman",
+    "Uploader": "<br>Uploaded by OsazuwaEmmanuelOsalotioman",
+    "Date": "2024-05-10",
+    "uni": "uniben",
+    "level": "200lv",
+    "dpt": "MTH"
+  }
 ];

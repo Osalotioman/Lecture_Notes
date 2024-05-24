267a75c346acd8e091409bd8caa4ab07fb1d746c
Osalotioman <osazuwaemmanuel25@gmail.com>
2024-05-24 04:29:00 +0100

    Rolling Back Updates

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Lecture_Notes
-A repository to share lecture notes (and any other useful materials) with those taking same courses with me at the University of Benin.
+# Lecture Notes
+
+A repository to share lecture notes and other useful materials with those taking the same courses at the University of Benin.
+
+## How to Upload notes
+
+You can upload notes by forking [the notes repository](https://github.com/Osalotioman/notes), uploading your notes, and making a pull request. Please ensure the file name has a good description so it can be easily added to the site.
+
+## How to add Announcements
+
+You van addd announcements by forking [this repository](https://github.com/Osalotioman/Lecture_Notea), using the template below write your announcement and put it in the announcement section im ann.html, then make a pull request.
+
+## Template for Announcements
+
+```html
+<div class="max-w-md bg-white p-3 rounded-lg shadow-md">
+    <ol>
+        <li class="mb-4">
+          <b style="color:blue;">Date announcement posted</b>
+          <br>
+          <span class="font-bold mb-4 text-2xl">
+            The title of your announcement will go here.
+          </span>
+        </li>
+        <li>
+          Details of announcements goes here.
+          <br>
+          <br>
+          <b style="color:green;">Written by ....</b>
+        </li>
+    </ol>
+</div>
--- app/i200lv.js
+++ app/i200lv.js
@@ -1,112 +1,17 @@
-var pages = ["#uni", "#levels", "#dpts", "#notes", "#form-section"];
-var pe = false;
-var tail = "";
-var uni_temp = `
-        <h2 class="text-2xl font-semibold mb-4 text-4xl">Universities</h2>
+var notes = `
+        <h2 class="text-2xl font-semibold mb-4 text-4xl">200 Level Lecture Notes</h2>
         <br>
-`;
-var levels = `
-        <h2 onclick="change_page('#uni', 'levels', false)" class="text-2xl font-semibold mb-4 text-4xl text-green-600">Back to Universities 🔙</h2>
-        <br>
-        <h2 class="text-2xl font-semibold mb-4 text-4xl">Levels</h2>
-        <br>
-`;
-var dpts_temp = `
-        <h2 onclick="change_page('#levels', 'dpts', false)" class="text-2xl font-semibold mb-4 text-4xl text-green-600">Back to Levels🔙</h2>
-        <br>
-        <h2 class="text-2xl font-semibold mb-4 text-4xl">Departments</h2>
-        <br>
-`;
-var uni_json = [
+    `
+/*var notes_json = [ 
   {
-    "name" : "Create Json",
-    "acronym" : "cj",
-    "info" : "For those of us who don't like the stress of typing when there's an alternative.",
-    "des" : "#form-section"
-  },
-  {
-    "name" : "University of Benin",
-    "acronym" : "uniben",
-    "info" : ""
-  },
-  {
-    "name" : "Benson Idahosa University",
-    "acronym" : "biu",
-    "info" : ""
-  }
-];
-var levels_json = [
-  {
-    "level" : "100 Level",
-    "page" : "100lv",
-    "info" : ""//"<br>Currently no writers(People to write scan and upload their notes). If interested message me on WhatsApp (+2348106052538)."
-  },
-  {
-    "level" : "200 Level",
-    "page" : "200lv",
-    "info" : ""//"<br>Need more writers(People to write scan and upload their notes). If interested message me on WhatsApp (+2348106052538)."
-  },
-  {
-    "level" : "300 Level",
-    "page" : "300lv",
-    "info" : ""
-  },
-  {
-    "level" : "400 Level",
-    "page" : "400lv",
-    "info" : ""
-  },
-  {
-    "level" : "500 Level",
-    "page" : "500lv",
-    "info" : ""
-  }
-];
-var dpts_json = [
-  {
-    "name" : "Mathematics",
-    "code" : "MTH",
-    "info" : ""
-  },
-  {
-    "name" : "Chemistry",
-    "code" : "CHM",
-    "info" : ""
-  },
-  {
-    "name" : "Computer Science",
-    "code" : "CSC",
-    "info" : ""
-  },
-  {
-    "name" : "Geology",
-    "code" : "GLY",
-    "info" : ""
-  },
-  {
-    "name" : "Statistics",
-    "code" : "STA",
-    "info" : ""
-  },
-  {
-    "name" : "Mechanical Engineering",
-    "code" : "ENG",
-    "info" : ""
-  }
-]
-var former_notes_json = [ 
-  {///dobe
     "Course" : "CSC211 - Structured Programming in Pascal",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_CSC211_05-15-2024.pdf",
     "Info" : "",
     "Lecturer" : "<br>Lecture delivered by Lecturer : Oliomogbe Silva",
     "Writer" : "<br>Written by OsazuwaEmmanuelOsalotioman",
-    "Date" : "15th May 2024",
-    "uni" : "uniben",
-    "level" : "200lv",
-    "dpt" : "CSC"
+    "Date" : "15th May 2024"
   },
-  {//done
+  {
     "Course" : "MTH214 - Introduction to Operations Research",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH214_05-15-2024.pdf",
     "Info" : "<br> <b style='color:red;'>The assignment given will be used for our Continuous Assessment</b>",
@@ -114,7 +19,7 @@ var former_notes_json = [
     "Writer" : "<br>Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "15th May 2024"
   },
-  {//done
+  {
     "Course" : "CSC212 - Symbolic Programming in Fortran",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_CSC212_05-15-2024.pdf",
     "Info" : " ",
@@ -122,7 +27,7 @@ var former_notes_json = [
     "Writer" : "<br>Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "15th May 2024"
   },
-  {//done
+  {
     "Course" : "MTH219 - Statistics",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH219_05-15-2024.pdf",
     "Info" : "<br> These are the note for MTH219 from the start of the class up until 14th May 2024",
@@ -130,7 +35,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by Jeffrey -CSC",
     "Date" : "15th May 2024"
   },
-  {//done
+  {
     "Course" : "MTH230 - Linear Algebra - Exercises",
     "PDF" : "notes/200lv/FirstSemester/Exercises_MTH230_05-14-2024.pdf",
     "Info" : "<br><b style='color:red;'>This is the exercises we are to focus on as part of our area of concentration for our test.</b>",
@@ -138,7 +43,7 @@ var former_notes_json = [
     "Writer" : "",
     "Date" : "14th May 2024"
   },
-  {//dobw
+  {
     "Course" : "MTH230 - Linear Algebra",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH230_05-14-2024.pdf",
     "Info" : "",
@@ -146,7 +51,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "14th May 2024"
   },
-  {//done
+  {
     "Course" : "MTH212 - Real Analysis",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH212_05-14-2024.pdf",
     "Info" : "<br><b style='color:red;'> There are Assignments/Classwork - A lot of assignments/classworks. The solution to the assignments/class work are to be written in a book or fuscap paper until it is requested for submission.</b>",
@@ -154,7 +59,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "14th May 2024"
   },
-  {//done
+  {
     "Course" : "MTH210(201) - Elementary Algebra and Analysis",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH210(201)_05-14-2024.pdf",
     "Info" : "",
@@ -162,7 +67,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "14th May 2024"
   },
-  {//done
+  {
     "Course" : "MTH218 - Mathematical Methods",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH218_05-13-2024.pdf",
     "Info" : "<br> <b style='color:red;'>Notes for the next MTH 218 Class</b>",
@@ -170,7 +75,7 @@ var former_notes_json = [
     "Writer" : "",
     "Date" : "13th May 2024"
   },
-  {//Done
+  {
     "Course" : "MTH230 - Linear Algebra",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH230_05-13-2024.pdf",
     "Info" : "<br> Set Theory <br> If you have a better (or more detailed) version of this notes and want to contribute, scroll to the bottom of the page",
@@ -178,7 +83,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "13th May 2024"
   },
-  {//Done
+  {
     "Course" : "CSC212 Symbolic Programming in Fortran",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_CSC212_05-13-2024.pdf",
     "Info" : "<br> <b style='color:red;'> Important!!!: There is assignment to be submitted next week Monday </b>",
@@ -186,7 +91,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "13th May 2024"
   },
-  {//Done
+  {
     "Course" : "MTH212 - Real Analysis",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH212_05-13-2024.pdf",
     "Info" : "<br> Limit of a Function",
@@ -194,7 +99,7 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "13th May 2024"
   },
-  {//done
+  {
     "Course" : "CHM213 - Practical Chemistry",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_CHM213_05-13-2024.pdf",
     "Info" : "<br> Collision Theory of Reaction Rate",
@@ -202,7 +107,7 @@ var former_notes_json = [
     "Writer" : "",
     "Date" : "13th May 2024"
   },
-  {//Done
+  {
     "Course" : "MTH 210(201) - Elementary Algebra and Analysis",
     "PDF" : "notes/200lv/FirstSemester/LectureNotes_MTH210(201)_10-05-2024_WrittenBy_Osalotioman.pdf",
     "Info": "<br> Introduction to Set Theory",
@@ -210,4 +115,25 @@ var former_notes_json = [
     "Writer" : "<br> Written by OsazuwaEmmanuelOsalotioman",
     "Date" : "10th May 2024"
   }
-];
+]*/
+for(notes_data of notes_json){
+  notes += `
+    <h3 class="text-lg font-medium mb-2 text-3xl">${notes_data["Course"]}</h3>
+        <div class="max-w-md bg-white p-4 rounded-lg shadow-md">
+            <ol id="notes_on_mth210(201)">
+                <li class="mb-4">
+                    <a href="${notes_data["PDF"]}"
+                        class="block text-blue-600 hover:underline">
+                        <span class="font-bold">${notes_data["Date"]}</span>
+                        ${notes_data["Info"]}
+                        ${notes_data["Lecturer"]}
+                        ${notes_data["Writer"]}
+                    </a>
+                </li>
+                <!-- Add more 200lv lecture notes here. -->
+            </ol>
+        </div>
+        <br><br>
+  `
+}
+$("#200lv").html(notes);

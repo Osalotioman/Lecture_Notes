b1514e9c4fe1126293cdce6330279e17d0ff5527
IghaloGenesisOsasenaga <ighalogenesis2007@gmail.com>
2024-11-13 12:35:56 +0100

    1st and 2nd semester Update #1: folder heirachy

---
+++ i200lv/first_semester/app/sections.js
@@ -0,0 +1,29 @@
+var notes_json = []
+var ass_json = []
+var sections_json = [
+  {
+    "name" : "Home",
+    "url" : "../../index.html",
+    "info" : ""
+  },
+  {
+    "name" : "Assignments",
+    "url" : "ass/index.html",
+    "info" : ""
+  },
+  {
+    "name" : "Lecture Notes",
+    "url" : "notes/index.html",
+    "info" : ""
+  },
+  {
+    "name" : "Code Snippets",
+    "url" : "cs/index.html",
+    "info" : "<b style='color:red;'>NEW!</b>"
+  },
+  {
+    "name" : "Past Questions",
+    "url" : "pq/index.html",
+    "info" : "<b style='color:red;'>NEW!</b>"
+  }
+];

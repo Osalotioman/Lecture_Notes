393aa91410000a51b70fc9da619e2528b604e30a
Osalotioman <109004799+Osalotioman@users.noreply.github.com>
2024-11-16 10:46:30 +0000

    Added number theory notes for second semester.

--- i200lv/second_semester/notes/app/njson.js
+++ i200lv/second_semester/notes/app/njson.js
@@ -19,4 +19,40 @@ var nip = "https://osalotioman.github.io/notes/"
 var sections_json = []
 var ass_json = []
 var notes_json = [
+  {
+    "Course" : "MTH240 - Number Theory.",
+    "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_22-10-24.pdf",
+    "Info" : "<br>This class covers an introduction to number theory and some basic definitions.",
+    "Lecturer" : "<br> Lecturer: Dr. K . Osawaru",
+    "Writer" : "<br> Written by Osalotioman",
+    "Date" : "22-10-2024",
+    "Uploader": "<br>Uploaded by Osalotioman",
+    "uni": "uniben",
+    "level": "200lv",
+    "dpt": "MTH"
+  },
+  {
+    "Course" : "MTH240 - Number Theory.",
+    "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_12-11-24.pdf",
+    "Info" : "<br>This class covers an the Greatest Common Divisor (GCD), Euclidean Algorithm(EA), and some other definitions.",
+    "Lecturer" : "<br> Lecturer: Dr. K . Osawaru",
+    "Writer" : "<br> Written by Osalotioman",
+    "Date" : "12-11-2024",
+    "Uploader": "<br>Uploaded by Osalotioman",
+    "uni": "uniben",
+    "level": "200lv",
+    "dpt": "MTH"
+  },
+  {
+    "Course" : "MTH240 - Number Theory.",
+    "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_13-11-24.pdf",
+    "Info" : "<br>This class covers Congruences, Congruences involving unkowns, and some other definitions.",
+    "Lecturer" : "<br> Lecturer: Dr. K . Osawaru",
+    "Writer" : "<br> Written by Osalotioman",
+    "Date" : "13-11-2024",
+    "Uploader": "<br>Uploaded by Osalotioman",
+    "uni": "uniben",
+    "level": "200lv",
+    "dpt": "MTH"
+  }
 ];

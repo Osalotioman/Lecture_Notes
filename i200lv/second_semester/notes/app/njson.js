7523d91ee299cb01b30ff1a9888af4faa952b76c
Osalotioman <109004799+Osalotioman@users.noreply.github.com>
2024-11-16 11:14:04 +0000

    Updated existing notes and added new note on abstract algebra first class

--- i200lv/second_semester/notes/app/njson.js
+++ i200lv/second_semester/notes/app/njson.js
@@ -22,7 +22,7 @@ var notes_json = [
   {
     "Course" : "MTH240 - Number Theory.",
     "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_22-10-24.pdf",
-    "Info" : "<br>This class covers an introduction to number theory and some basic definitions.",
+    "Info" : "<br>This class covers an introduction to number theory and some basic definitions, assignment on Euclidean Algorithm.(FirstClass)",
     "Lecturer" : "<br> Lecturer: Dr. K . Osawaru",
     "Writer" : "<br> Written by Osalotioman",
     "Date" : "22-10-2024",
@@ -34,7 +34,7 @@ var notes_json = [
   {
     "Course" : "MTH240 - Number Theory.",
     "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_12-11-24.pdf",
-    "Info" : "<br>This class covers an the Greatest Common Divisor (GCD), Euclidean Algorithm(EA), and some other definitions.",
+    "Info" : "<br>This class covers an the Greatest Common Divisor (GCD), Euclidean Algorithm(EA), and some other definitions, assignment on equivalence of congruence proof.(SecondClass)",
     "Lecturer" : "<br> Lecturer: Dr. K . Osawaru",
     "Writer" : "<br> Written by Osalotioman",
     "Date" : "12-11-2024",
@@ -46,8 +46,20 @@ var notes_json = [
   {
     "Course" : "MTH240 - Number Theory.",
     "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_13-11-24.pdf",
-    "Info" : "<br>This class covers Congruences, Congruences involving unkowns, and some other definitions.",
+    "Info" : "<br>This class covers Congruences, Congruences involving unkowns, and some other definitions.(ThirdClass)",
     "Lecturer" : "<br> Lecturer: Dr. K . Osawaru",
+    "Writer" : "<br> Written by Osalotioman",
+    "Date" : "13-11-2024",
+    "Uploader": "<br>Uploaded by Osalotioman",
+    "uni": "uniben",
+    "level": "200lv",
+    "dpt": "MTH"
+  },
+  {
+    "Course" : "MTH220 - Abstract Algebra.",
+    "PDF" : nip+"uniben/200lv/FirstSemester/MTH/MTH240_13-11-24.pdf",
+    "Info" : "<br>This class covers definition of a group, examples of groups, more examples on groups, assignment to show that some sets satisfy a group, PS. I have not properly documented subsequent classes due to the use of a digital material in them - Sad.(FirstClass)",
+    "Lecturer" : "<br> Lecturer: Dr. Seun",
     "Writer" : "<br> Written by Osalotioman",
     "Date" : "13-11-2024",
     "Uploader": "<br>Uploaded by Osalotioman",
